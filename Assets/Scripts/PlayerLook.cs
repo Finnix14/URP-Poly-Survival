fa72e617fb45014caad21e137f9d85887649d9c1
Finnix14 <49366542+Finnix14@users.noreply.github.com>
2022-08-30 02:28:37 +0100

    initial commit

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+
+
+    [Header("References")]
+ 
+
+    [SerializeField] public float sensX = 100f;
+    [SerializeField] public float sensY = 100f;
+
+    [SerializeField] Transform cam = null;
+    [SerializeField] public Transform orientation = null;
+
+    [SerializeField] Camera fpscam;
+    public PlayerLook look;
+
+    float mouseX;
+    float mouseY;
+
+    float multiplier = 0.01f;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        fpscam = Camera.main;
+        transform.rotation = Quaternion.Euler(mouseX, mouseY, transform.rotation.eulerAngles.z);
+
+    }
+
+    private void Update()
+    {
+
+
+
+
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRotation += mouseX * sensX * multiplier;
+        xRotation -= mouseY * sensY * multiplier;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cam.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.transform.rotation = Quaternion.Euler(0, yRotation, 0);
+
+    }
+        
+
+    
+
+
+}
